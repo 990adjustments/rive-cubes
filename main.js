8f15e286a457086281a2ca44345d90d898895959
Erwin Santacruz <hi@990adjustments.com>
2024-04-15 17:51:46 -0400

    Updated paths.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 import { setupCubes } from './rivesetup.js'
 
 document.querySelector('#app').innerHTML = `
-<div class="logo"><a href="/https://990adjustments.com/" target="_blank" rel="noopener noreferrer"><img src="/assets/logo01.png" alt="logo" class="retina"></a></div>
+<div class="logo"><a href="https://990adjustments.com/" target="_blank" rel="noopener noreferrer"><img src="/assets/logo01.png" alt="logo" class="retina"></a></div>
 <div>
     <h1>Rive Cubes</h1>
 </div>
